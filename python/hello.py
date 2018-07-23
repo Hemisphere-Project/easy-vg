05b6880e2f7d2623f10b55eb24a1ad75ed74b5f6
Mike Kazantsev <mk.fraggod@gmail.com>
2018-07-23 19:47:31 +0500

    python: add hello.py example

---
+++ python/hello.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Straightforward translation of hello.c example to python
+
+from evg import LibShapes
+
+evg = LibShapes()
+
+w, h = evg.c_int(), evg.c_int()
+evg.init(w, h)
+evg.begin()
+
+evg.background(0, 0, 0)
+
+evg.fill(44, 77, 232, 1)
+evg.circle(w.value / 2, 0, w.value)
+
+evg.fill(255, 255, 255, 1)
+evg.text_mid(
+	w.value / 2, h.value * 0.7,
+	b'hello world', None, int(w.value / 15) )
+
+evg.end()
+
+input()
+evg.finish()
